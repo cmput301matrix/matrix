acfcca1d33822ae9def58d12985f23dfeda9dc40
Subhana Khan <subhanakhan@Mac.lan>
2026-01-28 16:22:03 -0700

    Add Rectangle shape

---
+++ app/src/main/java/com/example/matrix/Rectangle.java
@@ -0,0 +1,13 @@
+package com.example.matrix;
+
+public class Rectangle extends Shape {
+    int width;
+    int height;
+
+    public Rectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
