66e70e7680bcb048c4b3b8fc977bcd4e0d4223bf
wangtaocheng <wangtaocheng@wangtaochengs-MacBook-Pro.local>
2026-01-29 17:40:37 -0700

    Add Circle Shape

---
+++ app/src/main/java/com/example/matrix/Circle.java
@@ -0,0 +1,11 @@
+package com.example.matrix;
+
+public class Circle extends Shape {
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+}
